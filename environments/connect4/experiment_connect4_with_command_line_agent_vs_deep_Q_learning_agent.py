af8f4b49bfd057cfa47071ae4aa70410e17c8099
lorgneschilooch <tripalium@gmail.com>
2019-10-17 15:21:51 +0200

    update experiment dql (add training) WARNING : Overflow with get_unique_id

--- environments/connect4/experiment_connect4_with_command_line_agent_vs_deep_Q_learning_agent.py
+++ environments/connect4/experiment_connect4_with_command_line_agent_vs_deep_Q_learning_agent.py
@@ -1,14 +1,24 @@
 from agents import CommandLineAgent, DeepQLearningAgent
 from environments.connect4 import Connect4GameState
-from runners import run_to_the_end
+from runners import run_to_the_end, run_for_n_games_and_print_stats, run_step
 
 if __name__ == "__main__":
     gs = Connect4GameState()
-    agent0 = CommandLineAgent()
+    agent0 = DeepQLearningAgent(action_space_size=gs.get_action_space_size())
     agent1 = DeepQLearningAgent(action_space_size=gs.get_action_space_size())
+    agent0.alpha = 0.1
+    agent0.epsilon = 0.005
     agent1.alpha = 0.1
     agent1.epsilon = 0.005
 
-    print(gs)
-    run_to_the_end([agent0, agent1], gs)
-    print(gs)
+    for i in range(100):
+        run_for_n_games_and_print_stats([agent0, agent1], gs, 5000)
+
+    agent0.epsilon = -1.0
+    agent1.epsilon = -1.0
+    run_for_n_games_and_print_stats([agent0, agent1], gs, 100)
+
+    gs_clone = gs.clone()
+    while not gs_clone.is_game_over():
+        run_step([agent0, CommandLineAgent()], gs_clone)
+        print(gs_clone)
