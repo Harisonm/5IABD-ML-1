64fb82178bda5daae59a0a9a07a154d2c8d12d16
lorgneschilooch <tripalium@gmail.com>
2019-10-18 22:51:53 +0200

    ADD : test for dql

--- environments/connect4/experiment_connect4_with_deep_Q_learning_vs_test.py
+++ environments/connect4/experiment_connect4_with_deep_Q_learning_vs_test.py
@@ -6,15 +6,12 @@ if __name__ == "__main__":
     gs = Connect4GameState()
     agent0 = DeepQLearningAgent(action_space_size=gs.get_action_space_size(), neurons_per_hidden_layer=256,
                                 hidden_layers=15)
-    agent1 = DeepQLearningAgent(action_space_size=gs.get_action_space_size(), neurons_per_hidden_layer=256,
-                                hidden_layers=15)
+    agent1 = RandomRolloutAgent(100, False)
     agent0.alpha = 0.1
     agent0.epsilon = 0.005
-    agent1.alpha = 0.1
-    agent1.epsilon = 0.005
 
     for i in range(100):
-        run_for_n_games_and_print_stats([agent0, agent1], gs, 500)
+        run_for_n_games_and_print_stats([agent0, agent1], gs, 1000)
 
     agent0.epsilon = -1.0
     run_for_n_games_and_print_stats([agent0, agent1], gs, 100)
